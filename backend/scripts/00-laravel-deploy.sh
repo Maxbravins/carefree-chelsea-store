c5f6d40aa7c4fae5fbd19e6308c9cf6fbcd6a0fa
Maxbravins <maxbravinso@gmail.com>
2026-08-24 23:46:19 +0300

    create admin user on deploy via env vars

--- backend/scripts/00-laravel-deploy.sh
+++ backend/scripts/00-laravel-deploy.sh
@@ -1,7 +1,5 @@
 set -euo pipefail
-
 cd /var/www/html
-
 mkdir -p \
   storage/app/private \
   storage/app/public \
@@ -11,15 +9,13 @@ mkdir -p \
   storage/framework/testing \
   storage/framework/views \
   bootstrap/cache
-
 chown -R nginx:nginx storage bootstrap/cache
 chmod -R ug+rwX storage bootstrap/cache
-
 php artisan migrate --force
 php artisan db:seed --class=ProductSeeder --force
+php artisan tinker --execute="\App\Models\User::firstOrCreate(['email' => env('ADMIN_EMAIL')], ['name' => 'Admin', 'password' => bcrypt(env('ADMIN_PASSWORD'))]);"
 php artisan optimize:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-
 chown -R nginx:nginx storage bootstrap/cache
