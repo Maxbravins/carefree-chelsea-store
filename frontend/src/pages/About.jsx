dbace2345dc2dd21b26a870e57af72e86fc2c002
Maxbravins <maxbravinso@gmail.com>
2026-09-11 13:02:01 +0300

    changes made in about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -92,7 +92,7 @@ export default function About() {
 
                         <div className="rounded-3xl overflow-hidden shadow-2xl">
                             <img
-                                src="/images/about-us.jpg"
+                                src="public/images/AboutUs.jpg"
                                 alt="About Us"
                                 className="w-full h-[420px] object-cover"
                             />
